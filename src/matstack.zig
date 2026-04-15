894aa58a2e076fa782b786029d4ad6bb0c5a5bb6
BradenEverson <bradeneverson@gmail.com>
2026-04-15 08:44:00 -0500

    Stubss

--- src/matstack.zig
+++ src/matstack.zig
@@ -7,9 +7,15 @@ pub const Operand = @import("operand.zig").Operand;
 
 pub const OperandStack = std.ArrayList(Operand);
 
+pub const NUM_REGISTERS: usize = 32;
+
+pub fn parseConstantPool(cpool_bytes: []const u8) []const Tensor {
+    _ = cpool_bytes;
+}
+
 pub const VirtualMachine = struct {
     constant_pool: []const Tensor,
-    gp_registers: [32]Tensor = undefined,
+    gp_registers: [NUM_REGISTERS]Tensor = undefined,
     stack: OperandStack,
 };
 
