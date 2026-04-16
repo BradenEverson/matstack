9fbcf1565200fe80a9dfa6dc87a1f5fe12d4e431
BradenEverson <bradeneverson@gmail.com>
2026-04-16 09:46:30 -0500

    Push pop stack

--- src/matstack.zig
+++ src/matstack.zig
@@ -9,9 +9,33 @@ pub const Instruction = @import("instruction.zig").Instruction;
 
 /// Maximum operands that can be on the stack at a time
 pub const MAX_STACK_SIZE: usize = 512;
+
+/// How many tensor slots there are for
+/// working area memory
 pub const SCRATCH_SIZE: usize = 256;
 
-pub const OperandStack = [MAX_STACK_SIZE]Operand;
+pub const StackError = error{
+    StackFull,
+    StackEmpty,
+};
+
+pub const OperandStack = struct {
+    sp: usize = 0,
+    stack: [MAX_STACK_SIZE]Operand,
+
+    pub fn push(stack: *OperandStack, val: Operand) !void {
+        if (stack.sp >= MAX_STACK_SIZE) return error.StackFull;
+        stack.stack[stack.sp] = val;
+        stack.sp += 1;
+    }
+
+    pub fn pop(stack: *OperandStack) !Operand {
+        if (stack.sp == 0) return error.StackEmpty;
+        stack.sp -= 1;
+
+        return stack.stack[stack.sp];
+    }
+};
 
 pub fn parseConstantPool(alloc: Allocator, cpool_bytes: []const u8) ![]const Tensor {
     _ = alloc;
