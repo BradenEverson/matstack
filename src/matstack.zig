df5bfe8eb7d9936f6d04a450876b9086f6ded662
BradenEverson <bradeneverson@gmail.com>
2026-04-16 09:59:38 -0500

    Split stack to new module

--- src/matstack.zig
+++ src/matstack.zig
@@ -4,38 +4,13 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 
 pub const Tensor = @import("tensor.zig");
+pub const OperandStack = @import("stack.zig");
 pub const Operand = @import("operand.zig").Operand;
 pub const Instruction = @import("instruction.zig").Instruction;
-
-/// Maximum operands that can be on the stack at a time
-pub const MAX_STACK_SIZE: usize = 512;
 
 /// How many tensor slots there are for
 /// working area memory
 pub const SCRATCH_SIZE: usize = 256;
-
-pub const StackError = error{
-    StackFull,
-    StackEmpty,
-};
-
-pub const OperandStack = struct {
-    sp: usize = 0,
-    stack: [MAX_STACK_SIZE]Operand,
-
-    pub fn push(stack: *OperandStack, val: Operand) !void {
-        if (stack.sp >= MAX_STACK_SIZE) return error.StackFull;
-        stack.stack[stack.sp] = val;
-        stack.sp += 1;
-    }
-
-    pub fn pop(stack: *OperandStack) !Operand {
-        if (stack.sp == 0) return error.StackEmpty;
-        stack.sp -= 1;
-
-        return stack.stack[stack.sp];
-    }
-};
 
 pub fn parseConstantPool(alloc: Allocator, cpool_bytes: []const u8) ![]const Tensor {
     _ = alloc;
@@ -52,4 +27,6 @@ pub const VirtualMachine = struct {
 test {
     _ = @import("tensor.zig");
     _ = @import("operand.zig");
+    _ = @import("instruction.zig");
+    _ = @import("stack.zig");
 }
---
+++ src/stack.zig
@@ -0,0 +1,29 @@
+//! The Operand Stack!
+
+pub const Operand = @import("operand.zig").Operand;
+
+/// Maximum operands that can be on the stack at a time
+pub const MAX_STACK_SIZE: usize = 512;
+
+pub const StackError = error{
+    StackFull,
+    StackEmpty,
+};
+
+sp: usize = 0,
+stack: [MAX_STACK_SIZE]Operand,
+
+const Self = @This();
+
+pub fn push(stack: *Self, val: Operand) !void {
+    if (stack.sp >= MAX_STACK_SIZE) return error.StackFull;
+    stack.stack[stack.sp] = val;
+    stack.sp += 1;
+}
+
+pub fn pop(stack: *Self) !Operand {
+    if (stack.sp == 0) return error.StackEmpty;
+    stack.sp -= 1;
+
+    return stack.stack[stack.sp];
+}
