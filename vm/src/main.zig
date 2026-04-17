216cbb55fdf347fb28e5bbe5f61579757ff72631
BradenEverson <bradeneverson@gmail.com>
2026-04-17 08:46:29 -0500

    Read from cli provided bytecode file

--- vm/src/main.zig
+++ vm/src/main.zig
@@ -1,6 +1,31 @@
 const std = @import("std");
 const matstack = @import("matstack");
+const VirtualMachine = matstack.VirtualMachine;
 
-pub fn main(init: std.process.Init) void {
-    _ = init;
+pub fn main(init: std.process.Init) !void {
+    const arena = init.arena;
+    const io = init.io;
+
+    const alloc = arena.allocator();
+
+    var args = init.minimal.args.iterate();
+    _ = args.next();
+
+    if (args.next()) |file_path| {
+        std.debug.print("Opening `{s}`\n", .{file_path});
+        const bytecode = try std.Io.Dir.cwd().readFileAlloc(
+            io,
+            file_path,
+            alloc,
+            .unlimited,
+        );
+        errdefer alloc.free(bytecode);
+
+        std.debug.print("{X}\n", .{bytecode});
+
+        var vm = try VirtualMachine.tryParse(alloc, bytecode);
+        while (!(try vm.step(arena.allocator()))) {}
+    } else {
+        std.debug.print("Missing bytecode file!!!\n", .{});
+    }
 }
