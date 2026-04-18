5c4ce7c02b56898e7b1b155d01a832b50e2936fe
BradenEverson <bradeneverson@gmail.com>
2026-04-18 18:14:47 -0500

    Validation step is gonna be a doozy

--- lang/src/compiler.zig
+++ lang/src/compiler.zig
@@ -1,2 +1,7 @@
 //! Translation from AST to VM Bytecode!
 
+pub const TranslationError = error{
+    /// when a provided tensor literal is inconsistently sized, such as
+    /// [[1,2,3], [1,2], [1,2,3]]
+    MalformedTensor,
+};
