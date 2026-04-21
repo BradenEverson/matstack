04f1cf378527cd532b27d9350504c774bdac7163
BradenEverson <bradeneverson@gmail.com>
2026-04-21 10:00:33 -0500

    Stack seems to be working!

--- hw/hw.vhd
+++ hw/hw.vhd
@@ -9,46 +9,52 @@ entity HW is
     port (
         CLK : in std_logic;
         RST : in std_logic;
-		SEL : in std_logic_vector(6 downto 0);
-		TENSOR: out std_logic_vector(31 downto 0)
+		  SEL : in std_logic_vector(6 downto 0);
+		  OP	: in std_logic_vector(1 downto 0);
+		  TENSOR: out std_logic_vector(31 downto 0);
+		  STACK_READY: out std_logic;
+		  STACK_VALID: out std_logic;
+		  STACK_ERR: out std_logic
     );
 end entity;
 
 architecture STRUCTURAL of HW is
 
-    signal stack_op      : std_logic_vector(1 downto 0) := "00";
     signal stack_push_in : tensor_t;
     signal stack_pop_out : tensor_t;
-    signal stack_ready   : std_logic;
-    signal stack_valid   : std_logic;
-    signal stack_err     : std_logic;
-	 
-	signal cpool_tensor	 : tensor_t;
-	signal cpool_valid 	 : std_logic;
-	signal en : std_logic;
-	 
-begin
-	CPOOL: entity work.CPOOL
-		port map (
-		  CLK	=> CLK,
-		  EN	=> RST,
-          ADDR => to_integer(unsigned(sel)),
-          DATA => cpool_tensor,
-          VALID => cpool_valid
-		);
+    signal sr   : std_logic;
+    signal sv   : std_logic;
+    signal se   : std_logic;
 
-	U_STACK : entity work.STACK
-	  port map (
-			CLK     => CLK,
-			RST     => RST,
-			OP      => stack_op,
-			PUSH => stack_push_in,
-			POP => stack_pop_out,
-			READY   => stack_ready,
-			VALID   => stack_valid,
-			ERR     => stack_err
-	  );
-	  
-	  
-    TENSOR <= cpool_tensor.data(0);
+    signal cpool_tensor  : tensor_t;
+    signal cpool_valid   : std_logic;
+
+begin
+    U_CPOOL : entity work.CPOOL
+        port map (
+            CLK   => CLK,
+            EN    => '1',
+            ADDR  => to_integer(unsigned(SEL)),
+            DATA  => cpool_tensor,
+            VALID => cpool_valid
+        );
+
+    U_STACK : entity work.STACK
+        port map (
+            CLK   => CLK,
+            RST   => RST,
+            OP    => OP,
+            PUSH  => stack_push_in,
+            POP   => stack_pop_out,
+            READY => sr,
+            VALID => sv,
+            ERR   => se
+        );
+
+    stack_push_in <= cpool_tensor;
+
+    TENSOR      <= stack_pop_out.data(0);
+    STACK_READY <= sr;
+    STACK_VALID <= sv;
+    STACK_ERR   <= se;
 end architecture;
