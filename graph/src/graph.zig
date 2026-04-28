a9128cdc523da1990ba51430a34b6706950ec210
BradenEverson <bradeneverson@gmail.com>
2026-04-28 10:06:36 -0500

    Insertion and inputs

--- graph/src/graph.zig
+++ graph/src/graph.zig
@@ -5,6 +5,7 @@ const Allocator = std.mem.Allocator;
 const Tensor = @import("matstack").Tensor;
 
 const Node = @import("node.zig");
+const NodeId = Node.NodeId;
 
 nodes: std.ArrayList(Node) = .empty,
 inputs: std.ArrayList(?Tensor) = .empty,
@@ -18,14 +19,27 @@ pub fn deinit(graph: *Graph, alloc: Allocator) void {
     }
     graph.nodes.deinit(alloc);
 
-    for (graph.inputs.items) |*input| {
-        if (input.*) |*i| i.deinit(alloc);
+    for (graph.inputs.items) |*in| {
+        if (in.*) |*i| i.deinit(alloc);
     }
     graph.inputs.deinit(alloc);
 }
 
 pub fn loadInput(graph: *Graph, idx: usize, val: Tensor) void {
     graph.inputs.items[idx] = val;
+}
+
+fn insert(graph: *Graph, alloc: Allocator, node: Node) !NodeId {
+    const id = NodeId{ .idx = graph.nodes.items.len };
+    try graph.nodes.append(alloc, node);
+    return id;
+}
+
+pub fn input(graph: *Graph, alloc: Allocator) !NodeId {
+    const input_id = graph.inputs.items.len;
+    const node = Node{ .ty = .{ .input = input_id } };
+
+    return try graph.nodes.append(alloc, node);
 }
 
 test {
