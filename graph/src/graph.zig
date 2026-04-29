c246dedb5448e8b3c104e4a10f8036b2a3057940
BradenEverson <bradeneverson@gmail.com>
2026-04-29 06:20:10 -0500

    All the ops as graph operations

--- graph/src/graph.zig
+++ graph/src/graph.zig
@@ -73,6 +73,76 @@ pub fn relu(graph: *Graph, alloc: Allocator, x: NodeId) !NodeId {
     });
 }
 
+pub fn sum(graph: *Graph, alloc: Allocator, x: NodeId) !NodeId {
+    const node_type: NodeType = .{ .operation = .{
+        .sum = .{ .x = x },
+    } };
+
+    return try graph.nodes.append(alloc, .{
+        .ty = .{ .operation = node_type },
+    });
+}
+
+pub fn add(graph: *Graph, alloc: Allocator, A: NodeId, B: NodeId) !NodeId {
+    const node_type: NodeType = .{ .operation = .{
+        .add = .{ .A = A, .B = B },
+    } };
+
+    return try graph.nodes.append(alloc, .{
+        .ty = .{ .operation = node_type },
+    });
+}
+
+pub fn regularization(graph: *Graph, alloc: Allocator, W: NodeId, epsilon: f32) !NodeId {
+    const node_type: NodeType = .{ .operation = .{
+        .regularization = .{ .W = W, .epsilon = epsilon },
+    } };
+
+    return try graph.nodes.append(alloc, .{
+        .ty = .{ .operation = node_type },
+    });
+}
+
+pub fn cross_entropy(graph: *Graph, alloc: Allocator, v: NodeId, y: NodeId) !NodeId {
+    const node_type: NodeType = .{ .operation = .{
+        .cross_entropy = .{ .v = v, .y = y },
+    } };
+
+    return try graph.nodes.append(alloc, .{
+        .ty = .{ .operation = node_type },
+    });
+}
+
+pub fn mse(graph: *Graph, alloc: Allocator, v: NodeId, y: NodeId) !NodeId {
+    const node_type: NodeType = .{ .operation = .{
+        .mse = .{ .v = v, .y = y },
+    } };
+
+    return try graph.nodes.append(alloc, .{
+        .ty = .{ .operation = node_type },
+    });
+}
+
+pub fn transpose(graph: *Graph, alloc: Allocator, X: NodeId) !NodeId {
+    const node_type: NodeType = .{ .operation = .{
+        .transpose = .{ .X = X },
+    } };
+
+    return try graph.nodes.append(alloc, .{
+        .ty = .{ .operation = node_type },
+    });
+}
+
+pub fn softmax(graph: *Graph, alloc: Allocator, x: NodeId) !NodeId {
+    const node_type: NodeType = .{ .operation = .{
+        .softmax = .{ .x = x },
+    } };
+
+    return try graph.nodes.append(alloc, .{
+        .ty = .{ .operation = node_type },
+    });
+}
+
 test {
     _ = @import("node.zig");
 }
